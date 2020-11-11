d1b55bb892f955bf0dc040bee9ece4e4d680ffd0
Dominik Kornat <dominik.kornat@antlis.pl>
2020-11-11 22:52:12 +0100

    Initial commit

---
+++ Lab1/Lab1/Lab1.DTO/Api/IPeopleClient.cs
@@ -0,0 +1,11 @@
+﻿using RestEase;
+using System.Threading.Tasks;
+
+namespace Lab1.DTO.Api
+{
+    public interface IPeopleClient
+    {
+        [Post("people")]
+        Task AddPersonAsync([Body] Person person);
+    }
+}
---
+++ Lab1/Lab1/Lab1/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Lab1.DTO.Api;
+
+namespace Lab1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var client = RestEase.RestClient.For<IPeopleClient>("http://192.168.8.103:6001/api");
+            InitializeComponent();
+
+            MainPage = new MainPage(client);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
