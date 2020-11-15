c13c3138e4f3c767f547d55c2c3769cfeb5db61b
Dominik Kornat <dominik.kornat@antlis.pl>
2020-11-15 17:58:46 +0100

    Initial commit of lab2

---
+++ Lab02/Lab02/Context/AzureDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Lab02.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab02.Context
+{
+    public class AzureDbContext : DbContext
+    {
+        public AzureDbContext(DbContextOptions<AzureDbContext> options) : base(options)
+        {
+
+        }
+
+        protected AzureDbContext()
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
